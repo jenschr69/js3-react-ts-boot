0057cbd7cf4f397503f7180579a15dde53f00ee7
Jens Christensen - Glodexa <jens@glodexa.com>
2024-11-06 11:09:46 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import './App.css';
-import { Counter from './components/counter' }
+import { Counter } from './components/counter';
 
 function App() {
   return (
     <div>
-      <Counter initValue={ 100 } />
+      <Counter initValue={ 100 } label='Räknare'/>
     </div>
   );
 }
 
 export default App;
+
--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,10 +1,16 @@
-
+import { useState } from "react";
 interface CounterProps {
     initValue?: number,
     label?: string,
 };
 
+interface CounterState {
+    counter: number
+}
+
 export const Counter = (props: CounterProps) => {
+    const initialState = { counter: props.initValue || 0 };
+    const [ state, setState] = useState < CounterState > (initialState);
 
     return (
         <div>
