671d250dd53e1753c9e6c0c568d6e7a9e6844ad2
Jens Christensen - Glodexa <jens@glodexa.com>
2024-11-06 11:21:42 +0100

    Update

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,21 +1,34 @@
 import { useState } from "react";
+
 interface CounterProps {
     initValue?: number,
-    label?: string,
+    label?: string
 };
 
 interface CounterState {
     counter: number
 }
 
+// { initValue } : { initValue?: number }
 export const Counter = (props: CounterProps) => {
-    const initialState = { counter: props.initValue || 0 };
-    const [ state, setState] = useState < CounterState > (initialState);
+
+    const initialState:CounterState = { counter: props.initValue || 0 };
+    const [ state, setState ] = useState<CounterState>(initialState);
+
+    const onClick = (increase: number) => {
+        setState( (oldState) => {
+            return {
+                counter: oldState.counter + increase
+            }
+        });
+    }
 
     return (
         <div>
             <h2>Counter Component</h2>
-            { props.label || "Counter" } : { props.initValue } <br />
+            { props.label || "Counter" }: { state.counter }<br />
+            <button onClick={ () => { onClick(5); } }>+5</button>
+            <button onClick={ () => { onClick(10); } }>+10</button>
         </div>
     );
 }
